b5743246c85b40472ca5f215b9d94e26be3dd2ff
MrTrotter95 <alistairtrotter522@gmail.com>
2022-06-11 17:32:23 +1200

    Work from home 11.06 - Added Login Functionality

--- 103.2-Group-Project/register.cpp
+++ 103.2-Group-Project/register.cpp
@@ -3,32 +3,33 @@
 
 class User {
 public:
+    //User Members
     int I_UserId = 1;
     char C_Gender = 'O';
     long int I_PhoneNum = 0;
-    int I_dob = 0;
-    string S_email = "";
-    string S_password = "";
+    string I_dob = " ";
+    string S_email = " ";
+    string S_password = " ";
     float F_discountValue = 0;
-    string S_contactMessage = "";
-    bool B_isComplaint = false;
-    bool B_isCompliment = false;
+    string S_contactMessage = " "; //This will store the users feedback.
+    bool B_isComplaint = false;  //If True, this will show in feedback report
+    bool B_isCompliment = false; //If True, this will show in feedback report
     float F_tab = 0;
-    int I_accessLevel = 1; // 1 Parent, 2 staff, 3 admin
+    int I_accessLevel = 1; // 1=Parent 2=Staff 3=Admin
 
     //Default Constructor
     User() {
     
     }
-    //Constructor
-    User(char gender, long int phoneNum, int dob, string email, string password, int accessLevel) {
+    
+    //Constructor with Arguments
+    User(char gender, long int phoneNum, string dob, string email, string password, int accessLevel) {
         C_Gender = gender;
         I_PhoneNum = phoneNum;
         I_dob = dob;
         S_email = email;
         S_password = password;
         I_accessLevel = accessLevel;
-        //implement the functionality to write to the csv file
     }
 
     void accountCreated() {
@@ -38,157 +39,179 @@ public:
     }
 };
 
-void saveLoginDetails(User account) {
+void SaveLoginDetails(User account) {
+    //Creating a instance of fstream & ifstream
     std::fstream fout;
+    std::ifstream  data("login.csv");
+    
+    // Temporary Variables
+    std::string id, line;
+    int newId;
 
-    std::string filename = "login.csv";
-    fout.open(filename, std::ios::out);
-
-
-
-    fout.close();
-
-    /*
-    // file pointer
-    std::fstream fout;
-    std::fstream fin;
-
-    // opens an existing csv file or creates a new file.
+    //Start of file writing logic
     fout.open("login.csv", std::ios::out | std::ios::app);
+    //This loops through each row in csv file and only checks the first cell of each row.
+    while (std::getline(data, line))
+    {
+        std::stringstream  lineStream(line);
+        getline(lineStream, id, ',');
+    }
 
-    fin.open("login.csv", std::ios::in);
+    //Converting string to int.
+    std::istringstream(id) >> newId;
+    //Takes last row in csv file and adds 1 to create a new unique ID.
+    newId += 1;
 
-    fin.is_open();
-    while (!fin.eof()) {
-        getline(fin, userId, ',');
-    }*/
-
-
-    fout << account.I_UserId << ", "
-         << account.S_email << ", "
-         << account.S_password << ", "
-         << account.I_accessLevel << "\n";
+    // Assigning the created account details to the csv file.
+    fout << newId << ","
+        << account.S_email << ","
+        << account.S_password << ","
+        << account.I_accessLevel << "\n";
+    fout.close();
+    //End of file writing logic
 }
 
-/* START OF CLASS USER PSUEDO CODE
+void SaveUserDetails(User account) {
+    //Creating a instance of fstream & ifstream
+    std::fstream fout;
+    std::ifstream  data("userDetails.csv");
 
-    Members include:
-• UserId: int
-• Gender: char
-• PhoneNum: long int?
-• DOB: int
-• Password: string
-• Email: string;
-• AccessLevel: int;
-• DiscountValue: float;
-• Children: null
-• ContactMessage: string
-• IsComplaint: bool
-• IsResolved: bool
-• Tab: float
+    // Temporary Variables
+    std::string id, line;
+    std::string::size_type sz;
+    int newId;
 
-    Constructors include:
-•Default Constructor
-•Constructor with parameters
+    //Start of file writing logic
+    fout.open("userDetails.csv", std::ios::out | std::ios::app);
+    //This loops through each row in csv file and only checks the first cell of each row.
+    while (std::getline(data, line))
+    {
+        std::stringstream  lineStream(line);
+        getline(lineStream, id, ',');
+    }
 
-    Functions include:
-•Login()
-    Create login atempt loop with max tries = 3
-    Display user instructions to user
-    Allow user to input details
-    store them in a varaible
-    check if user exists (if user doesn't exist diplay warning) (if user input is wrong key types display warning)
-    Check password is a match (is user input doesn't match display warning)
-    If password doesn't match increase counter by 1.
-    If counter == 3 execute function ForgotPass();
-    If Password does match display confirmation message.
-    Pass details into loggedIn() function.
-•ForgotPass()
-    Display message to user
-    Execute Main Menu Function
-END OF CLASS USER PSUEDO CODE*/
+    //Converting string to int.
+    std::istringstream(id) >> newId;
+    //Takes last row in csv file and adds 1 to create a new unique ID.
+    newId += 1;
 
-/*START OF PSUDEO CODE
-    Using a paramemter Constuctor from Class User,
-    Display user instructions to user
-    allow user to input detail
-    Include error checking to make sure the user inputs the correct key.
-    Store them in a temporary variable
-    Ask to confirm their password.
-    Compare both inputs to each other.
-    (if false) try again message.
-    (if true) continue.
-    Assign user input to the instance of the class
-    Save these values to the csv file?
-    Display a confirmation message to the user.
-    Execute the next function / open the relevant signed in menu (parent/admin).?? Potentially?
- END OF PSUDEO CODE*/
+    // Assigning the created account details to the csv file.
+    fout << newId << ","
+        << account.C_Gender << ","
+        << account.I_PhoneNum << ","
+        << account.I_dob << "\n";
+    fout.close();
+    //End of file writing logic
+}
 
-
-/*Start of Register functionality*/
+/*--- Start of Register functionality ---*/
 void registerMain()
 {
     cout << "register.cpp connected..\n";
 
-    // user Variables to handle user input for registration
+    /*--- Temporary Variables For Account Registration ---*/
     char input_Gender;
     long int input_PhoneNum;
-    int input_dob;
-    string input_email;
-    string input_password;
-    int userAccType; // To Determine what account they would like to create
-
-    int InputUserPin;
-    int adminPin = 1234;
+    string input_dob, day, month, year, input_email, input_password;
+    int userAccType;       // To Determine What Account They Would Like To Create
+    int InputUserPin;     // To Store User Input And Compare To AdminPin
+    int adminPin = 1234; //To Grant Access To Admin 
     
+    /*--- Start Of Menu/User Instructions To Register An Account ---*/
     cout << "What type of account would you like to create?\n1. Parent\n2. Staff\n3. Admin\n: ";
     cin >> userAccType;
     if (userAccType == 1) {
         cout << "--- Creating Parent account ---\n\n";
+
+
+        /*--- Gender ---*/
         cout << "--- Please select your gender ---\nFemale\nMale\nOther\n ";
-        cin >> input_Gender;
+        cin >> input_Gender; //***************Use Jordans menu select option for this input. ***************
 
-        cout << "\n--- Please enter your phone number ---\nNumber:";
+
+        /*--- Phone Number ---*/
+        cout << "\n--- Please enter your phone number ---\nNumber:"; //Would be a good idea to set a max input amoun allowed.
         cin >> input_PhoneNum;
+        while (std::cin.fail()) {
+            std::cout << "\nPlease enter only numeric values" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            std::cin >> input_PhoneNum;
+        }
 
-        cout << "\n--- Please enter your date of birth ---\nDOB:";
-        cin >> input_dob;
 
+        /*--- Date of birth ---*/
+        cout << "\n--- Please enter your date of birth ---\nExample dd/mm/yyyy:  "; //Need to work on this
+        cout << "Day: ";
+        cin >> day;
+        cout << "Month: ";
+        cin >> month;
+        cout << "Year: ";
+        cin >> year;
+        //cin >> input_dob;
+
+
+        /*--- Email ---*/
         cout << "\n--- Please enter your email address ---\nEmail:";
         cin >> input_email;
 
+
+        /*--- Password ---*/
         cout << "\n--- Please input your password ---\nPassword:";
         cin >> input_password;
 
-        //Pass the user input in the constructor.
+
+        /*--- Passing user input to constructor ---*/
         User account(input_Gender, input_PhoneNum, input_dob, input_email, input_password, userAccType);
-        saveLoginDetails(account);
+        
+        
+        /*--- Passing account to csv writing functions ---*/
+        SaveLoginDetails(account);
+        SaveUserDetails(account);
     }
     else if (userAccType == 2) {
         cout << "--- Creating Staff account ---\n\n";
-        cout << "--- Please select your gender ---\nFemale\nMale\nOther\n ";
-        cin >> input_Gender;
 
+
+        /*--- Gender ---*/
+        cout << "--- Please select your gender ---\nFemale\nMale\nOther\n ";
+        cin >> input_Gender; //Use Jordans menu select option for this input.
+
+
+        /*--- Phone Number ---*/
         cout << "\n--- Please enter your phone number ---\nNumber:";
         cin >> input_PhoneNum;
 
+
+        /*--- Date of birth ---*/
         cout << "\n--- Please enter your date of birth ---\nDOB:";
         cin >> input_dob;
 
+
+        /*--- Email ---*/
         cout << "\n--- Please enter your email address ---\nEmail:";
         cin >> input_email;
 
+
+        /*--- Password ---*/
         cout << "\n--- Please input your password ---\nPassword:";
         cin >> input_password;
 
-        //Pass the user input in the constructor.
+        /*--- Passing user input to constructor ---*/
         User account(input_Gender, input_PhoneNum, input_dob, input_email, input_password, userAccType);
-        saveLoginDetails(account);
+
+
+        /*--- Passing account to csv writing functions ---*/
+        SaveLoginDetails(account);
+        SaveUserDetails(account);
     }
     else if (userAccType == 3) {
+        /*--- Request Admin Pin ---*/
         cout << "Please enter pin: ";
         cin >> InputUserPin;
-        //While loop to ensure the userinput is a valid number
+        
+        
+        /*--- While loop to ensure the userinput is a valid number ---*/
         int counter = 0;
         while (std::cin.fail()) {
             std::cout << "\nPlease input a valid integer" << std::endl;
@@ -196,32 +219,50 @@ void registerMain()
             std::cin.ignore(256, '\n');
             std::cin >> InputUserPin;
         }
+
+        /*--- If user input is wrong ---*/
         if (InputUserPin != adminPin) {
             cout << "Pin is not correct, Please try again.\n";
             //Implement Functionality to try again / go back to main menu
         }
+        /*--- If user input is correct ---*/
         else if (InputUserPin == adminPin) {
             cout << "--- Pin entered correctly ---\n";
             cout << "--- Creating Admin Account ---\n";
-            cout << "--- Please select your gender ---\nFemale\nMale\nOther\n ";
-            cin >> input_Gender;
 
+
+            /*--- Gender ---*/
+            cout << "--- Please select your gender ---\nFemale\nMale\nOther\n ";
+            cin >> input_Gender; //Use Jordans menu select option for this input.
+
+
+            /*--- Phone Number ---*/
             cout << "\n--- Please enter your phone number ---\nNumber:";
             cin >> input_PhoneNum;
 
+
+            /*--- Date of birth ---*/
             cout << "\n--- Please enter your date of birth ---\nDOB:";
             cin >> input_dob;
 
+
+            /*--- Email ---*/
             cout << "\n--- Please enter your email address ---\nEmail:";
             cin >> input_email;
+            
 
+            /*--- Password ---*/
             cout << "\n--- Please input your password ---\nPassword:";
             cin >> input_password;
 
-            //Pass the user input in the constructor.
+
+            /*--- Passing user input to constructor ---*/
             User account(input_Gender, input_PhoneNum, input_dob, input_email, input_password, userAccType);
-            saveLoginDetails(account);
-            
+
+
+            /*--- Passing account to csv writing functions ---*/
+            SaveLoginDetails(account);
+            SaveUserDetails(account);
         }
     }
 
